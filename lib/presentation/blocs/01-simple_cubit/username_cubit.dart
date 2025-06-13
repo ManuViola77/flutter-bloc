e6ae4807446ee77b45fc16910d135b13a1b724e3
Manuela Viola <manuela.viola@gmail.com>
2025-06-13 10:37:19 -0300

    feat: set username

--- lib/presentation/blocs/01-simple_cubit/username_cubit.dart
+++ lib/presentation/blocs/01-simple_cubit/username_cubit.dart
@@ -2,4 +2,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class UsernameCubit extends Cubit<String> {
   UsernameCubit() : super('no-username');
+
+  void setUsername(String username) {
+    emit(username);
+  }
 }
